6b5974e121e9f83350561aaeeb021f6b23c83a30
Robin Millette <robin@millette.info>
2023-11-05 15:45:30 -0500

    Initial commit.

---
+++ astro.config.js
@@ -0,0 +1,15 @@
+// npm
+import solid from '@astrojs/solid-js'
+import tailwind from '@astrojs/tailwind'
+
+// astro
+import { defineConfig } from 'astro/config'
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    solid({
+      include: ['**/solid/*'],      
+    }),
+  ],
+})

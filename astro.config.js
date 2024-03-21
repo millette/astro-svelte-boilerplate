5a2e9e202817db75bb393c399d32575ec16328c1
Robin Millette <robin@millette.info>
2024-03-20 22:03:29 -0400

    Add breakpoint to dev bar.

--- astro.config.js
+++ astro.config.js
@@ -4,10 +4,12 @@ import tailwind from '@astrojs/tailwind'
 
 // astro
 import { defineConfig } from 'astro/config'
+import devtoolBreakpoints from "astro-devtool-breakpoints"
 
 export default defineConfig({
   integrations: [
     tailwind(),
+    devtoolBreakpoints(),
     solid({
       include: ['**/solid/*'],
     }),

bfeb789790bc8a0104c37e01a41cef1e5e6868f9
Robin Millette <robin@millette.info>
2024-12-09 07:08:53 -0500

    Remove breakpoint dev toolbar plugin.

--- astro.config.js
+++ astro.config.js
@@ -4,12 +4,10 @@ import tailwind from '@astrojs/tailwind'
 
 // astro
 import { defineConfig } from 'astro/config'
-import devtoolBreakpoints from "astro-devtool-breakpoints"
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    devtoolBreakpoints(),
     solid({
       include: ['**/solid/*'],
     }),

0b4ab208626ce0efa645b21e9f4e5dd328e507da
Robin Millette <robin@millette.info>
2024-12-17 14:21:13 -0500

    Add svelte astro integration.

--- astro.config.js
+++ astro.config.js
@@ -4,8 +4,8 @@ import tailwind from '@astrojs/tailwind'
 // astro
 import { defineConfig } from 'astro/config'
 
+import svelte from '@astrojs/svelte';
+
 export default defineConfig({
-  integrations: [
-    tailwind(),
-  ],
+  integrations: [tailwind(), svelte()],
 })
